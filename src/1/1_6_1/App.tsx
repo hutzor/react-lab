e14b9155ed7ee4242e40313417fa2161fcd3c3f9
Мария Соколова <>
2025-06-13 22:42:31 +0300

    1_6_1 Feat: added icon display for incomplete items

--- src/1/1_6_1/App.tsx
+++ src/1/1_6_1/App.tsx
@@ -1,31 +1,20 @@
-// 1_6_1 Show an icon for incomplete items with ? :
-/*
-  Используйте условный оператор (cond ? a : b) для отображения ❌, если isPacked не является true.
-*/
-
 function Item({ name, isPacked }: { name: string; isPacked: boolean }) {
   return (
-      <li className="item">
-          {name} {isPacked && '✔'}
-      </li>
+    <li className="item">
+      {name} {isPacked ? '✔' : '❌'}
+    </li>
   );
 }
 
 export default function PackingList() {
   return (
-      <section>
-          <h1>Sally Ride's Packing List</h1>
-          <ul>
-              <Item isPacked={true} name="Space suit" />
-              <Item
-                  isPacked={true}
-                  name="Helmet with a golden leaf"
-              />
-              <Item
-                  isPacked={false}
-                  name="Photo of Tam"
-              />
-          </ul>
-      </section>
+    <section>
+      <h1>Sally Ride&apos;s Packing List</h1>
+      <ul>
+        <Item name="Space suit" isPacked={true} />
+        <Item name="Helmet with a golden leaf" isPacked={true} />
+        <Item name="Photo of Tam" isPacked={false} />
+      </ul>
+    </section>
   );
 }
