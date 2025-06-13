44a84bdd3e0d90d4d7784391732a611b88e74e86
Мария Соколова <>
2025-06-13 22:59:01 +0300

    1_7_1 Refactor: split the list into two separate parts

--- src/1/1_7_1/App.tsx
+++ src/1/1_7_1/App.tsx
@@ -1,10 +1,3 @@
-// 1_7_1 Splitting a list in two 
-/*
-  В этом примере показан список всех людей.
-
-  Измените его, чтобы последовательно вывести два отдельных списка: Химики и Все остальные. Как и ранее, вы можете определить, является ли человек химиком, проверив, что person.profession === 'chemist'.
-*/
-
 import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
@@ -14,26 +7,36 @@ export type Person = {
   profession: string;
   accomplishment: string;
   imageId: string;
+};
+
+function PersonList({ title, people }: { title: string; people: Person[] }) {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>{' '}
+              {person.profession} known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default function List() {
-    const listItems = people.map((person) => (
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
-            </p>
-        </li>
-    ));
-    return (
-        <article>
-            <h1>Scientists</h1>
-            <ul>{listItems}</ul>
-        </article>
-    );
+  const chemists = people.filter((p) => p.profession === 'chemist');
+  const others = people.filter((p) => p.profession !== 'chemist');
+
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <PersonList title="Chemists" people={chemists} />
+      <PersonList title="Other scientists" people={others} />
+    </article>
+  );
 }
