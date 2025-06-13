f20f66d7b25c0500bf78c383b8938bb53ad592e9
Мария Соколова <>
2025-06-13 23:24:18 +0300

    1_8_3 Fix: prevented "Create Story" duplication by avoiding props mutation

--- src/1/1_8_3/App.tsx
+++ src/1/1_8_3/App.tsx
@@ -1,31 +1,26 @@
-// 1_8_3 Fix a broken story tray
-/*
-  Генеральный директор вашей компании просит вас добавить "истории" в ваше приложение онлайн-часов, и вы не можете отказать. Вы написали компонент StoryTray, который принимает список stories, за которым следует заполнитель "Create Story".
-
-  Вы реализовали заполнитель "Create Story", поместив еще одну фальшивую историю в конец массива stories, который вы получаете в качестве пропса. Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
-*/
-
 export interface Story {
   id: string;
   label: string;
-} 
-
-const stories: Story[] = [];
-export default function Wrapper(){
-  return <StoryTray stories={stories}/>;
 }
 
- function StoryTray({ stories }: { stories: Story[] }) {
-  stories.push({
-      id: 'create',
-      label: 'Create Story',
-  });
+const stories: Story[] = [];
+
+export default function Wrapper() {
+  return <StoryTray stories={stories} />;
+}
+
+function StoryTray({ stories }: { stories: Story[] }) {
+  // Формируем новый массив для рендера.
+  const trayStories: Story[] = [
+    ...stories,
+    { id: 'create', label: 'Create Story' }, 
+  ];
 
   return (
-      <ul>
-          {stories.map((story) => (
-              <li key={story.id}>{story.label}</li>
-          ))}
-      </ul>
+    <ul>
+      {trayStories.map((story) => (
+        <li key={story.id}>{story.label}</li>
+      ))}
+    </ul>
   );
 }
