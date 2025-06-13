b5ca73365e290ee3e66fa8c1d50516232f87b7c2
Мария Соколова <>
2025-06-13 23:12:17 +0300

    1_7_3 Refactor: extracted list item into a separate component

--- src/1/1_7_3/App.tsx
+++ src/1/1_7_3/App.tsx
@@ -1,28 +1,34 @@
-// 1_7_3 Extracting a list item component 
-/*
-  Этот компонент RecipeList содержит два вложенных вызова map. Чтобы упростить его, извлеките из него компонент Recipe, который будет принимать пропсы id, name и ingredients. Где вы разместите внешний key и почему?.
-*/
-
 import { recipes } from './data';
 
+type RecipeProps = {
+  name: string;
+  ingredients: string[];
+};
+
+function Recipe({ name, ingredients }: RecipeProps) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
-    return (
-        <div>
-            <h1>Recipes</h1>
-            {recipes.map((recipe) => (
-                <div key={recipe.id}>
-                    <h2>{recipe.name}</h2>
-                    <ul>
-                        {recipe.ingredients.map(
-                            (ingredient) => (
-                                <li key={ingredient}>
-                                    {ingredient}
-                                </li>
-                            )
-                        )}
-                    </ul>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.id}
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+        />
+      ))}
+    </div>
+  );
 }
