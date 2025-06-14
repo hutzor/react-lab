833560c8c4032a1633a5e29c77dee3091148096c
Мария Соколова <>
2025-06-14 20:15:16 +0300

    2_2_1 Feat: added "Previous" button and prevented index out-of-bounds in Gallery

--- src/2/2_2_1/App.tsx
+++ src/2/2_2_1/App.tsx
@@ -1,30 +1,36 @@
-// 2_2_1 Complete the gallery 
-/*
-  При нажатии кнопки "Next" на последней скульптуре происходит сбой кода. Исправьте логику, чтобы предотвратить сбой. Вы можете сделать это, добавив дополнительную логику в обработчик события или отключив кнопку, когда действие невозможно.
-
-  После устранения сбоя добавьте кнопку "Предыдущая", которая показывает предыдущую скульптуру. Она не должна разбиваться на первой скульптуре.
-*/
-
 import { useState } from 'react';
 import { sculptureList } from './data';
-
 
 export default function Gallery() {
     const [index, setIndex] = useState(0);
     const [showMore, setShowMore] = useState(false);
 
     function handleNextClick() {
-        setIndex(index + 1);
+        if (index < sculptureList.length - 1) {
+            setIndex(index + 1);
+        }
+    }
+
+    function handlePrevClick() {
+        if (index > 0) {
+            setIndex(index - 1);
+        }
     }
 
     function handleMoreClick() {
         setShowMore(!showMore);
     }
 
-    let sculpture = sculptureList[index];
+    const sculpture = sculptureList[index];
+
     return (
         <>
-            <button onClick={handleNextClick}>Next</button>
+            <button onClick={handlePrevClick} disabled={index === 0}>
+                Previous
+            </button>
+            <button onClick={handleNextClick} disabled={index === sculptureList.length - 1}>
+                Next
+            </button>
             <h2>
                 <i>{sculpture.name} </i>
                 by {sculpture.artist}
