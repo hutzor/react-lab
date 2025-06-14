e1d4542e3292f5698998b79174e95c717db6cfce
Мария Соколова <>
2025-06-14 23:48:11 +0300

    2_7_4 Refactor: removed mutations from todo handlers using Immer

--- src/2/2_7_4/AddTodo.tsx
+++ src/2/2_7_4/AddTodo.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 
-export default function AddTodo(
-    { onAddTodo }: { onAddTodo: (title: string) => void }) {
-    const [title, setTitle] = useState('');
-    return (
-        <>
-            <input
-                placeholder="Add todo"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <button
-                onClick={() => {
-                    setTitle('');
-                    onAddTodo(title);
-                }}
-            >
-                Add
-            </button>
-        </>
-    );
+export default function AddTodo({ onAddTodo }: { onAddTodo: (title: string) => void }) {
+  const [title, setTitle] = useState('');
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(e.target.value);
+  }
+
+  function handleAdd() {
+    onAddTodo(title);
+    setTitle('');
+  }
+
+  return (
+    <>
+      <input
+        placeholder="Add todo"
+        value={title}
+        onChange={handleChange}
+      />
+      <button onClick={handleAdd}>
+        Add
+      </button>
+    </>
+  );
 }
--- src/2/2_7_4/TaskList.tsx
+++ src/2/2_7_4/TaskList.tsx
@@ -2,81 +2,79 @@ import { useState } from 'react';
 import { Todo } from './App';
 
 export default function TaskList({
-    todos,
-    onChangeTodo,
-    onDeleteTodo,
+  todos,
+  onChangeTodo,
+  onDeleteTodo,
 }: {
-    todos: Todo[];
-    onChangeTodo: (nextTodo: Todo) => void;
-    onDeleteTodo: (todoId: number) => void;
+  todos: Todo[];
+  onChangeTodo: (nextTodo: Todo) => void;
+  onDeleteTodo: (todoId: number) => void;
 }) {
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
-                    <Task
-                        todo={todo}
-                        onChange={onChangeTodo}
-                        onDelete={onDeleteTodo}
-                    />
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <Task
+            todo={todo}
+            onChange={onChangeTodo}
+            onDelete={onDeleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-function Task(
-    { todo, onChange, onDelete }: {
-        todo: Todo;
-        onChange: (nextTodo: Todo) => void;
-        onDelete: (todoId: number) => void;
-    }
-) {
-    const [isEditing, setIsEditing] = useState(false);
-    let todoContent;
-    if (isEditing) {
-        todoContent = (
-            <>
-                <input
-                    value={todo.title}
-                    onChange={(e) => {
-                        onChange({
-                            ...todo,
-                            title: e.target.value,
-                        });
-                    }}
-                />
-                <button onClick={() => setIsEditing(false)}>
-                    Save
-                </button>
-            </>
-        );
-    } else {
-        todoContent = (
-            <>
-                {todo.title}
-                <button onClick={() => setIsEditing(true)}>
-                    Edit
-                </button>
-            </>
-        );
-    }
-    return (
-        <label>
-            <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={(e) => {
-                    onChange({
-                        ...todo,
-                        done: e.target.checked,
-                    });
-                }}
-            />
-            {todoContent}
-            <button onClick={() => onDelete(todo.id)}>
-                Delete
-            </button>
-        </label>
-    );
+function Task({
+  todo,
+  onChange,
+  onDelete,
+}: {
+  todo: Todo;
+  onChange: (nextTodo: Todo) => void;
+  onDelete: (todoId: number) => void;
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    onChange({ ...todo, title: e.target.value });
+  }
+
+  function handleDoneChange(e: React.ChangeEvent<HTMLInputElement>) {
+    onChange({ ...todo, done: e.target.checked });
+  }
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+  function handleDelete() {
+    onDelete(todo.id);
+  }
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={handleDoneChange}
+      />
+      {isEditing ? (
+        <>
+          <input value={todo.title} onChange={handleTitleChange} />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          {todo.title}
+          <button onClick={handleEdit}>Edit</button>
+        </>
+      )}
+      <button onClick={handleDelete}>Delete</button>
+    </label>
+  );
 }
