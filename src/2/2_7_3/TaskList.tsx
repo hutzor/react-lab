40d3f1564882964bf34a04234181ba08981f574e
Мария Соколова <>
2025-06-14 23:32:28 +0300

    2_7_3 Refactor: removed mutations from todo handlers for immutability

--- src/2/2_7_3/TaskList.tsx
+++ src/2/2_7_3/TaskList.tsx
@@ -2,81 +2,83 @@ import { useState } from 'react';
 import { Todo } from './App';
 
 export default function TaskList({
-    todos,
-    onChangeTodo,
-    onDeleteTodo,
+  todos,
+  onChangeTodo,
+  onDeleteTodo,
 }: {
-    todos: Todo[];
-    onChangeTodo: (nextTodo: Todo) => void;
-    onDeleteTodo: (todoId: number) => void;
+  todos: Todo[];
+  onChangeTodo: (nextTodo: Todo) => void;
+  onDeleteTodo: (todoId: number) => void;
 }) {
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
-                    <Task
-                        todo={todo}
-                        onChange={onChangeTodo}
-                        onDelete={onDeleteTodo}
-                    />
-                </li>
-            ))}
-        </ul>
-    );
+  // Сохраняем id задачи, которая сейчас редактируется
+  const [editingId, setEditingId] = useState<number | null>(null);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <Task
+            todo={todo}
+            isEditing={editingId === todo.id}
+            onEdit={() => setEditingId(todo.id)}
+            onSave={() => setEditingId(null)}
+            onChange={onChangeTodo}
+            onDelete={onDeleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-function Task(
-    { todo, onChange, onDelete }: {
-        todo: Todo;
-        onChange: (nextTodo: Todo) => void;
-        onDelete: (todoId: number) => void;
-    }
-) {
-    const [isEditing, setIsEditing] = useState(false);
-    let todoContent;
-    if (isEditing) {
-        todoContent = (
-            <>
-                <input
-                    value={todo.title}
-                    onChange={(e) => {
-                        onChange({
-                            ...todo,
-                            title: e.target.value,
-                        });
-                    }}
-                />
-                <button onClick={() => setIsEditing(false)}>
-                    Save
-                </button>
-            </>
-        );
-    } else {
-        todoContent = (
-            <>
-                {todo.title}
-                <button onClick={() => setIsEditing(true)}>
-                    Edit
-                </button>
-            </>
-        );
-    }
-    return (
-        <label>
-            <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={(e) => {
-                    onChange({
-                        ...todo,
-                        done: e.target.checked,
-                    });
-                }}
-            />
-            {todoContent}
-            <button onClick={() => onDelete(todo.id)}>
-                Delete
-            </button>
-        </label>
+function Task({
+  todo,
+  isEditing,
+  onEdit,
+  onSave,
+  onChange,
+  onDelete,
+}: {
+  todo: Todo;
+  isEditing: boolean;
+  onEdit: () => void;
+  onSave: () => void;
+  onChange: (nextTodo: Todo) => void;
+  onDelete: (todoId: number) => void;
+}) {
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          value={todo.title}
+          onChange={(e) => {
+            onChange({ ...todo, title: e.target.value });
+          }}
+        />
+        <button onClick={onSave}>Save</button>
+      </>
     );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        <button onClick={onEdit}>Edit</button>
+      </>
+    );
+  }
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={(e) => {
+          onChange({ ...todo, done: e.target.checked });
+        }}
+      />
+      {todoContent}
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </label>
+  );
 }
