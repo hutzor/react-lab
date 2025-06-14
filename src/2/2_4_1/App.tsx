09f8dd4c1fc07460f3b7b4a852fab2b4fbeefa10
Мария Соколова <>
2025-06-14 21:30:43 +0300

    2_4_1 Feat: Implement a traffic light

--- src/2/2_4_1/App.tsx
+++ src/2/2_4_1/App.tsx
@@ -1,33 +1,29 @@
-// 2_4_1 Implement a traffic light 
-/*
-  Здесь представлен компонент светофора для пешеходного перехода, который включается при нажатии на кнопку.
-
-  Добавьте alert в обработчик нажатия. Когда свет зеленый и говорит "Идти", щелчок по кнопке должен говорить "Следующая остановка". Когда свет красный и говорит "Стоп", нажатие на кнопку должно говорить "Следующим будет идти".
-
-  Есть ли разница в том, поместить ли alert до или после вызова setWalk?
-*/
-
 import { useState } from 'react';
 
 export default function TrafficLight() {
-    const [walk, setWalk] = useState(true);
+  const [walk, setWalk] = useState(true);
 
-    function handleClick() {
-        setWalk(!walk)
+  function handleClick() {
+    if (walk) {
+      alert('Следующая остановка');
+    } else {
+      alert('Следующим будет идти');
     }
+    setWalk(!walk);
+  }
 
-    return (
-        <>
-            <button onClick={handleClick}>
-                Change to {walk ? 'Stop' : 'Walk'}
-            </button>
-            <h1
-                style={{
-                    color: walk ? 'darkgreen' : 'darkred',
-                }}
-            >
-                {walk ? 'Walk' : 'Stop'}
-            </h1>
-        </>
-    );
+  return (
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1
+        style={{
+          color: walk ? 'darkgreen' : 'darkred',
+        }}
+      >
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  );
 }
