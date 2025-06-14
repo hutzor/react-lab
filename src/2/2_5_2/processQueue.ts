605d996b1fdc3f5a0010b37a03bd3c6f7f1fefa4
Мария Соколова <>
2025-06-14 21:53:02 +0300

    2_5_2 Feat: implement the state queue

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -1,9 +1,13 @@
 import { Que } from "./App";
 
-export function getFinalState(baseState: number, queue: Que[]) {
-    let finalState = baseState;
-
-    // TODO: do something with the queue...
-
-    return finalState;
+export function getFinalState(baseState: number, queue: Que[]): number {
+  let state = baseState;
+  for (const update of queue) {
+    if (typeof update === 'function') {
+      state = update(state);
+    } else {
+      state = update;
+    }
+  }
+  return state;
 }
