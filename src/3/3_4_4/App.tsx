1fc36bd59af4031ee531c3460e0c209aa5f2a5cc
Мария Соколова <>
2025-06-18 21:54:31 +0300

    3_4_4 Feat: cleared image placeholder while loading new image

--- src/3/3_4_4/App.tsx
+++ src/3/3_4_4/App.tsx
@@ -1,23 +1,21 @@
-// 3_4_4 Clear an image while it’s loading
-/*
-  Когда вы нажимаете кнопку "Далее", браузер начинает загрузку следующего изображения. Однако, поскольку оно отображается в том же теге img, по умолчанию вы будете видеть предыдущее изображение, пока не загрузится следующее. Это может быть нежелательно, если важно, чтобы текст всегда совпадал с изображением. Измените это так, чтобы при нажатии кнопки "Next" предыдущее изображение сразу же убиралось.
-*/
-
 import { useState } from 'react';
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
   const hasNext = index < images.length - 1;
 
   function handleClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
+    setIndex(hasNext ? index + 1 : 0);
+    setLoading(true);
   }
 
-  let image = images[index];
+  function handleLoad() {
+    setLoading(false);
+  }
+
+  const image = images[index];
+
   return (
     <>
       <button onClick={handleClick}>
@@ -26,33 +24,24 @@ export default function Gallery() {
       <h3>
         Image {index + 1} of {images.length}
       </h3>
-      <img src={image.src} />
-      <p>
-        {image.place}
-      </p>
+      {loading && <p>Loading...</p>}
+      <img
+        src={image.src}
+        onLoad={handleLoad}
+        style={{ display: loading ? 'none' : 'block' }}
+        alt={image.place}
+      />
+      {!loading && <p>{image.place}</p>}
     </>
   );
 }
 
-let images = [{
-  place: 'Penang, Malaysia',
-  src: 'FJeJR8M.jpg'
-}, {
-  place: 'Lisbon, Portugal',
-  src: 'dB2LRbj.jpg'
-}, {
-  place: 'Bilbao, Spain',
-  src: 'z08o2TS.jpg'
-}, {
-  place: 'Valparaíso, Chile',
-  src: 'Y3utgTi.jpg'
-}, {
-  place: 'Schwyz, Switzerland',
-  src: 'JBbMpWY.jpg'
-}, {
-  place: 'Prague, Czechia',
-  src: 'QwUKKmF.jpg'
-}, {
-  place: 'Ljubljana, Slovenia',
-  src: '3aIiwfm.jpg'
-}];
+const images = [
+  { place: 'Penang, Malaysia', src: 'FJeJR8M.jpg' },
+  { place: 'Lisbon, Portugal', src: 'dB2LRbj.jpg' },
+  { place: 'Bilbao, Spain', src: 'z08o2TS.jpg' },
+  { place: 'Valparaíso, Chile', src: 'Y3utgTi.jpg' },
+  { place: 'Schwyz, Switzerland', src: 'JBbMpWY.jpg' },
+  { place: 'Prague, Czechia', src: 'QwUKKmF.jpg' },
+  { place: 'Ljubljana, Slovenia', src: '3aIiwfm.jpg' }
+];
