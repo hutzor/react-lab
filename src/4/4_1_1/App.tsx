a85a5673d0335a2cf7536f1c6d2956cc0cbf6428
Мария Соколова <>
2025-06-19 19:54:56 +0300

    4_1_1 Fix: allowed message sending to be canceled properly

--- src/4/4_1_1/App.tsx
+++ src/4/4_1_1/App.tsx
@@ -1,19 +1,13 @@
-// 4_1_1  Fix a broken chat input
-/*
-  Если ввести сообщение и нажать "Отправить" то перед появлением сообщения "Отправлено!" произойдет трехсекундная задержка. Кнопка "Отменить" должна остановить появление сообщения "Отправлено!". Она делает это, вызывая clearTimeout для идентификатора таймаута, сохраненного во время handleSend. Однако даже после нажатия кнопки "Отменить" сообщение "Отправлено!" все равно появляется. Найдите причину неработоспособности и устраните ее.
-*/
-
-import { useState } from 'react';
-import { getImageUrl } from "./utils"
+import { useRef, useState } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
   const [isSending, setIsSending] = useState(false);
-  let timeoutID = null;
+  const timeoutID = useRef<number | null>(null);
 
   function handleSend() {
     setIsSending(true);
-    timeoutID = setTimeout(() => {
+    timeoutID.current = window.setTimeout(() => {
       alert('Отправлено!');
       setIsSending(false);
     }, 3000);
@@ -21,7 +15,9 @@ export default function Chat() {
 
   function handleUndo() {
     setIsSending(false);
-    clearTimeout(timeoutID);
+    if (timeoutID.current !== null) {
+      clearTimeout(timeoutID.current);
+    }
   }
 
   return (
@@ -29,18 +25,16 @@ export default function Chat() {
       <input
         disabled={isSending}
         value={text}
-        onChange={e => setText(e.target.value)}
+        onChange={(e) => setText(e.target.value)}
       />
-      <button
-        disabled={isSending}
-        onClick={handleSend}>
+
+      <button disabled={isSending} onClick={handleSend}>
         {isSending ? 'Отправляем...' : 'Отправить'}
       </button>
-      {isSending &&
-        <button onClick={handleUndo}>
-          Отменить
-        </button>
-      }
+
+      {isSending && (
+        <button onClick={handleUndo}>Отменить</button>
+      )}
     </>
   );
 }
