f60a1915eaf16c4f2c61de1692162614176c2fe9
Мария Соколова <>
2025-06-20 10:56:57 +0300

    4_8_1 Refactor: extract a useCounter Hook

--- src/4/4_8_1/useCounter.ts
+++ src/4/4_8_1/useCounter.ts
@@ -1,5 +1,15 @@
+import { useState, useEffect } from 'react';
 
 export function useCounter() {
-    // Тут нужно реализовать хук
-    return 0
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return count;
 }
