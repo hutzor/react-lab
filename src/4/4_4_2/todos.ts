a2dfe613f6ab155242412e389615177a0878f563
Мария Соколова <>
2025-06-20 09:41:17 +0300

    4_4_2 Refactor: replaced useEffect with useMemo to cache visibleTodo

--- src/4/4_4_2/todos.ts
+++ src/4/4_4_2/todos.ts
@@ -4,27 +4,25 @@ export type Todo = {
   completed: boolean;
 };
 
-
 let nextId = 0;
 let calls = 0;
 
-export function getVisibleTodos(todos: Todo[], showActive: boolean) {
-console.log(`getVisibleTodos() was called ${++calls} times`);
-const activeTodos = todos.filter(todo => !todo.completed);
-const visibleTodos = showActive ? activeTodos : todos;
-return visibleTodos;
+export function getVisibleTodos(todos: Todo[], showActive: boolean): Todo[] {
+  console.log(`getVisibleTodos() was called ${++calls} times`);
+  const activeTodos = todos.filter(todo => !todo.completed);
+  return showActive ? activeTodos : todos;
 }
 
 export function createTodo(text: string, completed = false): Todo {
-return {
-  id: nextId++,
-  text,
-  completed
-};
+  return {
+    id: nextId++,
+    text,
+    completed,
+  };
 }
 
 export const initialTodos: Todo[] = [
-createTodo('Get apples', true),
-createTodo('Get oranges', true),
-createTodo('Get carrots'),
+  createTodo('Get apples', true),
+  createTodo('Get oranges', true),
+  createTodo('Get carrots'),
 ];
